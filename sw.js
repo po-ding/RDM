f6dd670df45cc4ecf369d3374fda94cb75dc66bf
po-ding <146317694+po-ding@users.noreply.github.com>
2025-08-09 16:46:44 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // PWA 설치를 위한 최소한의 서비스 워커
 self.addEventListener('fetch', (event) => {
-  // 현재는 특별한 캐싱 전략 없이 네트워크 요청을 그대로 통과시킵니다.
+  // 네트워크 요청에만 의존
   event.respondWith(fetch(event.request));
 });
